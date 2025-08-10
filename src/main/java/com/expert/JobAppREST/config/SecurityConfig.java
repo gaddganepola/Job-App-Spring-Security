db031ef73a7b5866b9bed10a7f468357dc65a521
gaddganepola <ganeypola@gmail.com>
2025-08-10 08:25:56 +0530

    create template for custom security configuration

---
+++ src/main/java/com/expert/JobAppREST/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.expert.JobAppREST.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+}
