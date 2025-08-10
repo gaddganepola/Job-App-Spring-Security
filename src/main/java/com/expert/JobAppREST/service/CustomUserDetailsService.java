66eb008ce7c53f22a9a483283c0ce209338881a1
gaddganepola <ganeypola@gmail.com>
2025-08-10 09:02:57 +0530

    develop logic for find user by username

--- src/main/java/com/expert/JobAppREST/service/CustomUserDetailsService.java
+++ src/main/java/com/expert/JobAppREST/service/CustomUserDetailsService.java
@@ -1,5 +1,9 @@
 package com.expert.JobAppREST.service;
 
+import com.expert.JobAppREST.model.User;
+import com.expert.JobAppREST.model.UserPrincipal;
+import com.expert.JobAppREST.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,8 +11,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo repo;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = repo.findUserByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new UserPrincipal(user);
     }
 }
